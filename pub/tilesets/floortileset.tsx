e53a2763886a5ac2c0d6288bf89d6fcb6ceab76a
jpreisner <jpreisner@free.fr>
2021-02-01 15:22:29 +0000

    Deploying to gh-pages from master @ d92bb41c18c8a615ca1b2b1a72c0254e6f2747c0 🚀

---
+++ pub/tilesets/floortileset.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="floortileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <properties>
+  <property name="collidable" type="bool" value="true"/>
+ </properties>
+ <image source="floortileset.png" width="256" height="256"/>
+ <tile id="37">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
